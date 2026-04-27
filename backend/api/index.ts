774e7d3e672277f6dc3efc4493369312e7a4a97a
IGIRANEZA Dominique <igdominik250@gmail.com>
2026-04-27 09:42:34 +0200

    Update index.ts

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,3 +1,4 @@
+import express from "express";
 import { createApp } from "../src/app.js";
 import { connectDb, registerMongoReadyHandler } from "../src/config/db.js";
 import { bootstrapMongoData } from "../src/services/bootstrap.service.js";
@@ -10,4 +11,4 @@ connectDb().catch(console.error);
 
 const app = createApp();
 
-export default app;
+module.exports = app;
